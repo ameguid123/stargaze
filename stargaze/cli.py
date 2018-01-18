684021b2d4ca0f238bebe32cf4e1144c76ff2864
ameguid123 <adham_meguid@college.harvard.edu>
2018-01-18 11:13:47 -0500

    Feat(Geolocation): Geolocate User from IP and Create PyEphem Observer Get user's location from request to freegeoip.net. Use PyEphem to create an "observer" to be used for viewing info from the user's location. Created "sun_test" to test the observer works correctly; sun_test outputs the sun's altitude/azimuth and rise/noon/set time for a given PyEphem observer.

--- stargaze/cli.py
+++ stargaze/cli.py
@@ -1,2 +1,32 @@
+import json
+import requests
+import ephem
+from datetime import datetime
 def main():
-    pass
+    usr = create_observer(get_location())
+    sun_test(usr)
+
+def get_location():
+    """Get user's latitude and longitude using freegeoip.net"""
+    req = (requests.get('http://freegeoip.net/json'))
+    loc = json.loads(req.text)
+    return (str(loc['latitude']), str(loc['longitude']))
+
+def create_observer(usr_coords):
+    """Create PyEphem observer using user latitude/longitude and UTC time"""
+    usr = ephem.Observer()
+    usr.lat, usr.lon = usr_coords
+    usr.date = datetime.utcnow()
+    return usr
+
+def sun_test(obs):
+    """Sun's altitude/azimuth and rise/noon/set time for PyEphem observer"""
+    sun = ephem.Sun()
+    sun.compute(obs)
+    print("sun altitude: %s, sun azimuth: %s" % (sun.alt, sun.az))
+    print("sun rise time: %s" % obs.previous_rising(sun))
+    print("solar noon: %s "% obs.next_transit(sun))
+    print("sunset time: %s" % obs.next_setting(sun))
+ 
+if __name__ == "__main__":
+    main()
