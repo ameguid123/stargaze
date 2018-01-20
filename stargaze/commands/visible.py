cc80f7de46922dcf3e91af07008f2cb43cc101a6
ameguid123 <adham_meguid@college.harvard.edu>
2018-01-19 21:18:09 -0500

    Feat(Visible): Basic visible Command Functional If user is in astronomical twilight and the planet is above the user's horizon, info about the planet's position is displayed in green. If the sun is up or the planet is not above the horizon, the planet's position is displayed in red. If in civil/nautical twilight and the planet is above the horizon its position info is displayed in yellow. Plans to make this a lot cleaner, and add a marker to show if the planet is rising or setting, but this works for now.

--- stargaze/commands/visible.py
+++ stargaze/commands/visible.py
@@ -1,11 +1,32 @@
 from .command_template import CommandTemplate
 import ephem
 import math
+from colorama import Fore
+
+
 class Visible(CommandTemplate):
     """List celestial objects visible from the user's location"""
     def run(self):
-        deg_per_rad = 180.0 / math.pi
+        deg_to_rad = 180.0 / math.pi
+        sun = ephem.Sun()
+        sun.compute(CommandTemplate.usr)
+        sun_angle = sun.alt * deg_to_rad
+        sun_status = Fore.RED
+        # Civil twilight
+        if sun_angle <= -6:
+            sun_status = Fore.YELLOW
+            # Nautical twilight
+            if sun_angle <= -12:
+                # Astronomical twilight
+                if sun_angle <= -18:
+                    sun_status = Fore.GREEN
         for planet in self.planets:
             obj = getattr(ephem, planet)(CommandTemplate.usr)
-            print("%s: altitude: %4.1f deg, azimuth: %5.1f deg" 
-                     % (planet, obj.alt * deg_per_rad, obj.az * deg_per_rad))
+            info_str = ('%s: altitude: %4.1f deg, azimuth: %5.1f deg'
+                        % (planet, obj.alt * deg_to_rad, obj.az * deg_to_rad))
+            if sun_status == Fore.RED:
+                print(sun_status + info_str + ', sun\'s still up')
+            elif obj.alt * deg_to_rad < 0:
+                print(Fore.RED + info_str + ', planet beneath horizon')
+            else:
+                print(sun_status + info_str)
